cd36343dc9355988d85ce754ea22601c6539fe15
kensuke goto <utogoken2@yahoo.co.jp>
2019-09-15 21:17:03 +0900

    dispathされた際の内容を出力

--- src/reducers/count.js
+++ src/reducers/count.js
@@ -3,7 +3,7 @@ import { INCREMENT,DECREMENT } from '../actions'
 const initialState = { value: 0 }
 
 export default (state = initialState,action) => {
-
+  console.log(action)
   switch(action.type){
     case INCREMENT:
       return {value: state.value + 1}
